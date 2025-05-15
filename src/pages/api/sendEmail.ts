7449c4cfce774188c32b21669567d9994a1a122c
Khaled-Alabadla <kh.es.abadla@gmail.com>
2025-05-15 14:09:50 +0300

    new commit

--- src/pages/api/sendEmail.ts
+++ src/pages/api/sendEmail.ts
@@ -13,7 +13,7 @@ export default async function handler(
 
   try {
     const data = await resend.emails.send({
-      from: "khaled-esam@gmail.com",
+      from: "test@gmail.com",
       to: "kh.es.abadla@gmail.com",
       subject: `New message from ${name}: ${subject}`,
       reply_to: email,
